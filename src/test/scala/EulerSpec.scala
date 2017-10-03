ac1d98535c211c0c128fb3bea3af32ccdfce3e2b
Emily St <hello@emily.st>
2017-10-03 05:02:00 +0000

    Write up about problem 206 (solution coming)

--- src/test/scala/EulerSpec.scala
+++ src/test/scala/EulerSpec.scala
@@ -9,8 +9,6 @@ class EulerSpec extends FunSuite with TimeLimitedTests {
   val timeLimit = Span(1, Minutes)  // enforce one-minute rule
 
   /**
-   * Project Euler Problem 99
-   *
    * > Comparing two numbers written in index form like 2^11 and 3^7 is
    * > not difficult, as any calculator would confirm that 2^11 = 2048
    * > < 3^7 = 2187.
@@ -52,7 +50,7 @@ class EulerSpec extends FunSuite with TimeLimitedTests {
    * Once I cleared up my own misconception about the (ir)relevance of the
    * logarithmic base, it was easy to proceed.
    */
-  test("solve problem 99") {
+  test("Project Euler Problem 99") {
     val (_, lineNumber) = Source
       .fromResource("p099_base_exp.txt")
       .getLines
@@ -66,5 +64,41 @@ class EulerSpec extends FunSuite with TimeLimitedTests {
       }
 
     println(s"Problem 99: Largest number is on line '${lineNumber + 1}'")
+  }
+
+  /**
+   * > Find the unique positive integer whose square has the form
+   * > 1_2_3_4_5_6_7_8_9_0, where each “_” is a single digit.
+   *
+   * All right. That's a nineteen-digit number whose square root is an
+   * integer. Can't be too many of those, right? My first impression is
+   * actually that I can brute-force the solution by squaring all
+   * numbers starting from, oh, let's say, 1000000000 (which would put
+   * us in the neighborhood, at 1e18), but we're still an order of
+   * magnitude off, and there's actually a lot of numbers up here.
+   *
+   * I have a suspicion that the ending in zero is significant.
+   * I imagine only certain numbers can be squared such that the final
+   * digit is zero. That limits the brute-forcing space significantly if
+   * I identify those numbers.
+   *
+   * On thinking about it, I suspect that only numbers ending in zero
+   * can have squares ending in zero. But those squares always end in
+   * zero-zero. So this mystery number must end in 900. Which means that
+   * the root must end in 30 or 70. We can eliminate other
+   * possibilities, so we've dramatically reduced the search space.
+   *
+   * I suspect that similar patterns extend up to the higher digits. I'm
+   * tired and have a headache and don't want to think about it. I'm
+   * going to look up the solution now.
+   *
+   * Having looked it up, it seems that there is no more clever solution
+   * than what I have found, so I have just to implement it. I will do
+   * this soon.
+   */
+  test("Project Euler Problem 206") {
+    // do {
+
+    // } while
   }
 }
